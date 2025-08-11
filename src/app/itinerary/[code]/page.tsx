564b5020fe2f933df7da362bf9554aa4faae7256
Charlie Wu <Charliewu500@gmail.com>
2025-08-11 16:41:47 +0800

    [add] 增加頁面

--- src/app/itinerary/[code]/page.tsx
+++ src/app/itinerary/[code]/page.tsx
@@ -4,24 +4,25 @@ import React from "react";
 import { Container, DialogTitle, Paper, Input, Button } from "@mui/material";
 import Link from "next/link";
 
-export default function Home() {
-  const [id, setId] = React.useState('');
+type Props = {
+	params: {
+		code: string;
+	}
+};
 
-  return (
-    <Container sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+export default function Itinerary({ params }: any) {
+	const code = params.code
+
+	return (
+	<Container sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
       <Paper sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 2, p: 4}}>
         <DialogTitle>
-          歡迎來到行程頁，請輸入行程 ID
+          歡迎來到 {code} 行程
         </DialogTitle>
-        <Input
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-          placeholder="請輸入文字"
-        />
-        <Link href={`/itinerary/${id}`}>
+        <Link href={`/`}>
           <Button variant="contained">前往頁面</Button>
         </Link>
       </Paper>
     </Container>
-  );
+	)
 }
